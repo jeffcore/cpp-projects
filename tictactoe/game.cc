ad39fcaaaacaccd7661ac54102d5085a31d00cdb
jeffcore <rixempire@gmail.com>
2020-02-06 18:26:25 -0600

    Added check for tie game and Computer AI for defensive blocks

--- tictactoe/game.cc
+++ tictactoe/game.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 std::string double_down = "\n\n";
+enum Player {COMPUTER, HUMAN};
 
 /* 
     prints game board with current state
@@ -31,16 +32,78 @@ bool valid_move(char *board, int position) {
 }
 
 /* 
+   computer ai for defensive block
+   parameters: the board
+   return: int - position to block
+*/
+int computer_ai_block(char *board, Player player_1) {
+    char opponent_symbol = '_';
+
+    if (player_1 == HUMAN) {
+        opponent_symbol = 'X';
+    } else {
+        opponent_symbol = 'O';
+    }
+
+    if ((board[0] == board[1]) && (board[1] == opponent_symbol) && (board[2] == '_')) {
+        return 2;
+    } else if ((board[0] == board[2]) && (board[2] == opponent_symbol) && (board[1] == '_')) {
+        return 1;
+    } else if ((board[1] == board[2]) && (board[2] == opponent_symbol) && (board[0] == '_')) {
+        return 0;
+    } else if ((board[3] == board[4]) && (board[4] == opponent_symbol) && (board[5] == '_')) {
+        return 5;
+    } else if ((board[3] == board[5]) && (board[5] == opponent_symbol) && (board[4] == '_')) {
+        return 4;
+    } else if ((board[4] == board[5]) && (board[5] == opponent_symbol) && (board[3] == '_')) {
+        return 3;
+    } else if ((board[6] == board[7]) && (board[7] == opponent_symbol) && (board[8] == '_')) {
+        return 8;
+    } else if ((board[6] == board[8]) && (board[8] == opponent_symbol) && (board[7] == '_')) {
+        return 7;
+    } else if ((board[7] == board[8]) && (board[8] == opponent_symbol) && (board[6] == '_')) {
+        return 6;
+    } else if ((board[0] == board[3]) && (board[3] == opponent_symbol) && (board[6] == '_')) {
+        return 6;
+    } else if ((board[0] == board[6]) && (board[6] == opponent_symbol) && (board[3] == '_')) {
+        return 3;
+    } else if ((board[3] == board[6]) && (board[6] == opponent_symbol) && (board[0] == '_')) {
+        return 0;
+    } else if ((board[1] == board[4]) && (board[4] == opponent_symbol) && (board[7] == '_')) {
+        return 7;
+    } else if ((board[1] == board[7]) && (board[7] == opponent_symbol) && (board[4] == '_')) {
+        return 4;
+    } else if ((board[4] == board[7]) && (board[7] == opponent_symbol) && (board[1] == '_')) {
+        return 1;
+    } else if ((board[2] == board[5]) && (board[5] == opponent_symbol) && (board[8] == '_')) {
+        return 8;
+    } else if ((board[2] == board[8]) && (board[8] == opponent_symbol) && (board[5] == '_')) {
+        return 5;
+    } else if ((board[5] == board[8]) && (board[8] == opponent_symbol) && (board[2] == '_')) {
+        return 2;
+    } 
+    return -1;
+}
+
+/* 
    computer logic for picking position - currently just random
    parameters: the board
    return: position
 */
-int computer_turn(char *board) {
-    int position = rand() % 9;
-    while (!valid_move(board, position)) {
-        position = rand() % 9; 
+int computer_turn(char *board, Player player_1) {
+    int defensive_move = -1;
+    defensive_move = computer_ai_block(board, player_1);
+    if (defensive_move == -1) {
+        int position = rand() % 9;
+        while (!valid_move(board, position)) {
+            position = rand() % 9; 
+        }
+        std::cout << position;
+        return position;
+    } else {
+        std::cout << defensive_move;
+        return defensive_move;
     }
-    return position;
 }
 
 /* 
@@ -70,16 +133,46 @@ char check_winner(char *board) {
     }
 }
 
+/* 
+   check board for tie
+   parameters: the board
+   return: true or false
+*/
+char check_tie(char *board) {    
+    if (board[0] == '_') {
+        return false;
+    } else if (board[1] == '_') {
+        return false;
+    } else if (board[2] == '_') {
+        return false;
+    } else if (board[3] == '_') {
+        return false;
+    } else if (board[4] == '_') {
+        return false;
+    } else if (board[5] == '_') {
+        return false;
+    } else if (board[6] == '_') {
+        return false;
+    } else if (board[7] == '_') {
+        return false;
+    } else if (board[8] == '_') {
+        return false;
+    }
+    return true;
+}
+
 int main(int argc, const char * argv[]) {
-    enum player {COMPUTER, HUMAN};
-    player player_1 = HUMAN;
-    player current_player = HUMAN;
+    
+    Player player_1 = HUMAN;
+    Player current_player = HUMAN;
     std::string go_first_answer = "";    
     int position = -1;
     char board[9] = {'_','_','_','_','_','_','_','_','_'};
     bool game_over = false;
     bool move_completed = false;
     char current_symbol = 'X';
+    char winner = '_';
+    bool is_tie = false;
 
     print_welcome();
     std::cout << "Do you want to go first? \n (y / n) >> ";
@@ -98,7 +191,7 @@ int main(int argc, const char * argv[]) {
             current_symbol = 'O';
         }
         if (current_player == COMPUTER) {  
-            board[computer_turn(board)] = current_symbol;
+            board[computer_turn(board, player_1)] = current_symbol;
             current_player = HUMAN;
             std::cout << "Computer Turn" << std::endl;
         } else {            
@@ -118,11 +211,19 @@ int main(int argc, const char * argv[]) {
             }
         }
         print_board(board);   
-        char winner = check_winner(board);        
+        winner = check_winner(board); 
+             
         if (winner != '_') {
             std::cout << "winner is " << winner << std::endl;
             game_over = true;
-        }    
+        }  
+        
+        is_tie = check_tie(board);  
+        if (is_tie == true) {
+            std::cout << "Tie Game" << std::endl;
+            game_over = true;
+        }
+
     }
     
     return 0;
